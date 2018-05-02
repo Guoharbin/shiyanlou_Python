06e4c81fc1bcb3242c8571beff0c34c6dbf70ed3
= <=>
2018-05-02 23:40:54 +0800

    类与Collection模块

--- 类/student_teacher.py
+++ 类/student_teacher.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-
+import sys
+import collections
+from collections import Counter
 class Person(object):
     """
     返回具有给定名称的 Person 对象
@@ -17,37 +19,50 @@ class Person(object):
 
 class Student(Person):
     """
-    返回 Student 对象，采用 name，branch，year 3个参数
+    返回 Student 对象，采用 name, branch, year 3 个参数
     """
-    def __init__(self, name, branch, year):
-         Person.__init__(self, name)
-         self.branch = branch
-         self.year = year
+
+    def __init__(self, name, branch, year,grade):
+        Person.__init__(self, name)
+        self.branch = branch
+        self.year = year
+        self.grade = Counter(grade)
 
     def get_details(self):
         """
         返回包含学生具体信息的字符串
         """
         return "{} studies {} and is in {} year.".format(self.name, self.branch, self.year)
-
+    def get_grade(self):
+        num = sum(self.grade.values())
+        numD = self.grade['D']
+        return "Pass: {}, Fail: {}".format(num-numD, numD)
 
 class Teacher(Person):
     """
     返回 Teacher 对象，采用字符串列表作为参数
     """
-    def __init__(self, name, papers):
+    def __init__(self, name, papers,grade):
         Person.__init__(self, name)
         self.papers = papers
+        self.grade = Counter(grade).most_common()
 
     def get_details(self):
         return "{} teaches {}".format(self.name, ','.join(self.papers))
+    
+    def get_grade(self):
+        s = ''
+        for k, v in self.grade:
+            s += k + ': ' + str(v) + ', '
+        return s[:-2]
 
-
-person1 = Person('Sachin')
-student1 = Student('Kusha1', 'CSE', 2005)
-teacher1 = Teacher('Prashed', ['C', 'C++'])
-
-print(person1.get_details())
-print(student1.get_details())
-print(teacher1.get_details())
-
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        if sys.argv[1] == 'teacher':
+            teacher1 = Teacher('harbin', ['c'], sys.argv[2])
+            print(teacher1.get_grade())
+        elif sys.argv[1] == 'student':
+            student1 = Student('student','cse',2005, sys.argv[2])
+            print(student1.get_grade())
+    else:
+        print(len(sys.argv))
