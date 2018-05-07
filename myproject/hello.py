361ba4b4d1843ff870d2813eb26e1d9a906e0bca
= <=>
2018-05-07 20:46:41 +0800

    flask

---
+++ myproject/hello.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/shiyanlou')
+def helloshiyanlou():
+    return 'Guohaobin'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
+
+@app.route('/sum/<int:a>/<int:b>')
+def sum_a_b(a,b):
+    return '%d' % (a+b)
+
+if __name__ == '__main__':
+    app.debug
+    app.run()
